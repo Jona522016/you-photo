85918a904e33cc3ac3b1d7fafa32190021806956
EmanuelGuerra <developer@ideasysistemas.com>
2021-01-12 08:20:04 -0600

    sw static-cache-v3

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v4';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v2';
-const INMUTABLE_CACHE = 'inmutable-v2';
+const INMUTABLE_CACHE = 'inmutable-v1';
 
 
 const APP_SHELL = [
